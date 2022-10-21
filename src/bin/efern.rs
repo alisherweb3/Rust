159f8235f21c9d7f9b3d4b1cdf21e4fffce66829
alisherweb3 <42702073+alisherweb3@users.noreply.github.com>
2022-10-21 16:02:04 +0500

    Create efern.rs

---
+++ src/bin/efern.rs
@@ -0,0 +1,12 @@
+extern crate fern_sim;
+use fern-sim::{Fern, run_simulation};
+
+fn main() {
+  let mut fern = Fern {
+    size: 1.0,
+    growth_rate: 0.001
+  };
+  run_simulation(&mut fern, 1000);
+
+  Println!("конечный размер папоротника: {}", fern_size);
+}
